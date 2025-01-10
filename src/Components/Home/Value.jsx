cc09fc621676adb8d4003c5ed07bda72e12abbbe
md-ahmodullah <contactme.ahmod@gmail.com>
2025-01-11 04:03:26 +0600

    Added Value Section

---
+++ src/Components/Home/Value.jsx
@@ -0,0 +1,34 @@
+import { FaShareSquare } from "react-icons/fa";
+import { FaUserGroup } from "react-icons/fa6";
+import { FcCollaboration } from "react-icons/fc";
+import { GrResources } from "react-icons/gr";
+export default function Value() {
+  return (
+    <div className="w-10/12 mx-auto py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+      <div className="flex items-center gap-2">
+        <div className="bg-red-200 p-2 rounded-full">
+          <GrResources className="text-xl text-black" />
+        </div>
+        <p className="text-xl text-black font-bold">5k+ Resources</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="bg-gray-200 p-2 rounded-full">
+          <FcCollaboration className="text-xl text-white" />
+        </div>
+        <p className="text-xl text-black font-bold">Easy Collaboration</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="bg-blue-100 p-2 rounded-full">
+          <FaShareSquare className="text-xl text-black" />
+        </div>
+        <p className="text-xl text-black font-bold">Knowledge Sharing</p>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="bg-purple-200 p-2 rounded-full">
+          <FaUserGroup className="text-xl text-black" />
+        </div>
+        <p className="text-xl text-black font-bold">Pro-active Groups</p>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Components/Home/Banner";
 import FAQ from "../Components/Home/FAQ";
 import Features from "../Components/Home/Features/Features";
+import Value from "../Components/Home/value";
 
 export default function Home() {
   return (
     <div className="font-roboto">
       <Banner />
+      <Value />
       <div className="w-10/12 mx-auto my-0 lg:my-12 py-8">
         <Features />
       </div>
